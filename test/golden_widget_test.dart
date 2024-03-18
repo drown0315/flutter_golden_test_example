b9b955bc28dbda028da37d8c305966fdb6f8996c
drown0315 <drown0315@gmail.com>
2024-03-18 18:27:37 +0800

    fix: 更新基准图片

--- test/golden_widget_test.dart
+++ test/golden_widget_test.dart
@@ -4,6 +4,6 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   testWidgets('Circle Button Golden test', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
-    await expectLater(find.byType(MyApp), matchesGoldenFile('circle_button.png'));
+    await expectLater(find.byType(MyApp), matchesGoldenFile('circle_button_01.png'));
   });
 }
